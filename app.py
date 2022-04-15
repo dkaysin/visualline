f2766691a5ab9aaf7485427ffc8bf752afd913df
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2022-04-15 22:21:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, session
 import imageio.v3 as iio
 import io
 import requests as req
 from os import environ
+from markupsafe import escape
 
 
 from process import get_strips, draw, render
 from data import sample_dataset, get_data
 
 app = Flask(__name__)
+app.secret_key = b'sf423ef24378y'
 
 APP_ID = "1113503899492527"
 FB_VISUALLINE_APP_SECRET = environ.get('FB_VISUALLINE_APP_SECRET')
-AUTH_REDIRECT_URL = "https://visualline.herokuapp.com/auth/"
-FB_AUTH_URL = "https://api.instagram.com/oauth/access_token"
+APP_URL = "https://visualline.herokuapp.com/"
+AUTH_REDIRECT_URL = APP_URL+"auth/"
+FB_AUTH_URL = "https://api.instagram.com/oauth/authorize"
+FB_ACCESS_TOKEN_URL = "https://api.instagram.com/oauth/access_token"
+FB_GRAPH_URL = "https://graph.instagram.com/"
+
+
+# Initialize SQLite3
 
 
 # This will be moved to frontend
 @app.route("/login/")
 def request_login():
-    href = "https://api.instagram.com/oauth/authorize" + \
-        f"?client_id={APP_ID}" + \
-        f"&redirect_uri={AUTH_REDIRECT_URL}" + \
-        "&scope=user_profile,user_media" + \
-        "&response_type=code"
+    href = f"""{FB_AUTH_URL}
+        ?client_id={APP_ID}
+        &redirect_uri={AUTH_REDIRECT_URL}
+        &scope=user_profile,user_media"
+        &response_type=code"""
     return f"<html> <a href={href}> Click to login </a> </html>"
 
 
 @app.route("/auth/")
 def auth():
-    code = request.args.get('code')
+    code = escape(request.args.get('code'))
     fields = {
         'client_id': APP_ID,
         'client_secret': FB_VISUALLINE_APP_SECRET,
@@ -37,15 +45,32 @@ def auth():
         'redirect_uri': AUTH_REDIRECT_URL,
         'code': code
     }
-    response = req.post(FB_AUTH_URL, data=fields)
+    response = req.post(FB_ACCESS_TOKEN_URL, data=fields).json()
+    print("Auth response:")
     print(response)
-    return f"<html> Your short lived auth code is: {code}</html>"
+    # Save user's credentials in session storage
+    session['user_id'] = response['user_id']
+    session['access_token'] = response['access_token']
+    return f"<html> Authentication is complete</html>"
 
-
-# @app.route("/", methods=['GET'])
 @app.route("/")
+def index():
+    return f"""
+        <html>
+            <a href="{APP_URL}fetch/">/fetch/</a> <br/>
+            <a href="{APP_URL}login/">/login/</a> <br/>
+            <a href="{APP_URL}auth/">/auth/</a>
+        </html>
+        """
+
+
+@app.route("/fetch/")
 def serve_image():
     # account_name = request.args.get('acc')
+
+    # if 'user_id' in session:
+    #     return f'Logged in as {session["username"]}'
+    # return 'You are not logged in'
 
     # images, timestamps = get_data(account_name)
     images, timestamps = get_data("")
@@ -56,6 +81,14 @@ def serve_image():
     iio.imwrite(output, canvas, format_hint=".jpg")
     output.seek(0)
     return send_file(output, mimetype='image/jpg')
+
+
+def fetch_data():
+    user_id = ""
+    access_token = ""
+    r = f"""{FB_GRAPH_URL}/{user_id}?
+        fields=id,username
+        &access_token={access_token}"""
 
 
 if __name__ == '__main__':
