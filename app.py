e07c32667ccdf2920085a3f94938199efa2f65af
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2022-04-15 18:33:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, send_file
+from main import dataset, get_strips, draw, render
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    images, timestamps = dataset()
+    strips = get_strips(images)
+    canvas = draw(strips, timestamps)
+    render(canvas)
+
+    # return "<p>Hello, World!</p>"
+    image = os.path.join('./out.jpg')
+    return send_file(image, mimetype='image/jpg')
