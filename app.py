90c81e5b7c064bdaf48f538f9da231d0cb23235a
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2022-04-15 21:09:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_file, request
 import imageio.v3 as iio
 import io
+import requests as req
 
 from process import get_strips, draw, render
 from data import sample_dataset, get_data
@@ -8,7 +9,9 @@ from data import sample_dataset, get_data
 app = Flask(__name__)
 
 APP_ID = "1113503899492527"
-REDIRECT_URL = "https://visualline.herokuapp.com/auth/"
+APP_SECRET = '01bbb3e9102acf6212348e9fff0a36a4'
+AUTH_REDIRECT_URL = "https://visualline.herokuapp.com/auth/"
+FB_AUTH_URL = "https://api.instagram.com/oauth/access_token"
 
 
 # This will be moved to frontend
@@ -16,17 +19,24 @@ REDIRECT_URL = "https://visualline.herokuapp.com/auth/"
 def request_login():
     href = "https://api.instagram.com/oauth/authorize" + \
         f"?client_id={APP_ID}" + \
-        f"&redirect_uri={REDIRECT_URL}" + \
+        f"&redirect_uri={AUTH_REDIRECT_URL}" + \
         "&scope=user_profile,user_media" + \
         "&response_type=code"
-    # send request
     return f"<html> <a href={href}> Click to login </a> </html>"
 
 
 @app.route("/auth/")
 def auth():
     code = request.args.get('code')
-    print(code)
+    fields = {
+        'client_id': APP_ID,
+        'client_secret': APP_SECRET,
+        'grant_type': 'authorization_code',
+        'redirect_uri': AUTH_REDIRECT_URL,
+        'code': code
+    }
+    response = req.post(FB_AUTH_URL, data=fields)
+    print(response)
     return f"<html> Your short lived auth code is: {code}</html>"
 
 
