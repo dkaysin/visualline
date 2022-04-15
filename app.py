dcc641522814b7215afdda74a441e267bb8b6a68
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2022-04-15 20:49:53 +0300

    Add FB auth integration

--- app.py
+++ app.py
@@ -7,11 +7,32 @@ from data import sample_dataset, get_data
 
 app = Flask(__name__)
 
+APP_ID = 321034639960041
+REDIRECT_URL = "https://visualline.herokuapp.com/auth/"
+
+
+# This will be moved to frontend
+@app.route("/login/")
+def request_login():
+    request = "https://api.instagram.com/oauth/authorize" + \
+        f"?client_id = {APP_ID}" + \
+        f"& redirect_uri = {REDIRECT_URL}" + \
+        "& scope = user_profile, user_media" + \
+        "& response_type = code"
+    # send request
+
+
+@app.route("/auth/<code>")
+def auth():
+    code = request.args.get('code')
+    print(code)
+    return f"<html> Your short lived auth code is: {code}</html>"
+
 
 # @app.route("/", methods=['GET'])
 @app.route("/")
 def serve_image():
-    # account_name = request.args.get['acc']
+    # account_name = request.args.get('acc')
 
     # images, timestamps = get_data(account_name)
     images, timestamps = get_data("")
@@ -24,6 +45,6 @@ def serve_image():
     return send_file(output, mimetype='image/jpg')
 
 
-# if __name__ == '__main__':
-#     # print('Test')
-#     image = serve_image()
+if __name__ == '__main__':
+    print('Hello world!')
+    # image = serve_image()
